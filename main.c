55ebf71ff3959b805d80cf49a5a2e14791abbe18
seocoal <jiyoon369@sookmyung.ac.kr>
2023-12-07 15:51:31 +0900

    2

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -10,31 +11,19 @@ struct Book{
 };
 
 int main(int argc, char *argv[]) {
-	int i;
-	struct Book**bookshelf;
 	
-	bookshelf=(struct Book**)malloc(sizeof(struct Book*));
-	for(i=0;i<3;i++)
-	  bookshelf[i]= malloc(10*sizeof(struct Book));
-	  
-	bookshelf[1][3].number=5;
-	strcpy(bookshelf[1][3].title, "C++ Programming");
-	
-	(bookshelf[2]+4)->number=3;
-	strcpy((bookshelf[2]+4)->title, "Communications Theory");
-	
-	printf("book(1,3): %i, %s\n", (bookshelf[1]+3)->number, (bookshelf[1]+3) ->title );
-	printf("book(2,4): %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title);
-	
-	
-	for (i=0;i<3;i++)
-	   free(bookshelf[i]);
-	   
-	free(bookshelf);
-	    
-
-
-
+    struct Book *p;
+	p= (struct Book*)malloc(2*sizeof(struct Book));
 	
 
+	p->number=1;
+	strcpy(p->title, "C programming");
+	
+	(p+1)->number=2;
+	strcpy((p+1)->title, "Electronics");
+	
+	free(p);
+	return;
+	
+	
 }
